4148a9222b1cf8a2476fb1d970fd9e887873f105
Tair-X <you@example.com>
2021-10-01 20:24:09 +0300

    Ioy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+let dfo=()=>{
+  localStorage(setTimeout(() => {
+    timeout++
+  }, timeout))
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
