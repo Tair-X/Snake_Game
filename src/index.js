d3198780077c11cbe261de984720543d48b03d44
Tair-X <you@example.com>
2021-10-01 20:18:33 +0300

    lol

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const [state, setstate] = React.useState(initialState)
+
+const bot=()=>{setstate(state+1)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
